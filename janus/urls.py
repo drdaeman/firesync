ca35b53c2575fc037030a851e141ef42bb30725b
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2015-06-01 12:29:56 +0300

    Add "force_auth" endpoint (currently, an alias to signin view)

--- janus/urls.py
+++ janus/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns("",
     url(r"^signup$", "janus.views.page_signup"),
     url(r"^signin$", "janus.views.page_signin"),
+    url(r"^force_auth", "janus.views.page_signin"),
 
     url(r"^v1/account/create$", "janus.views.account_create"),
     url(r"^v1/account/status$", "janus.views.account_status"),
