17630c925f3113016eebf2acdf2e8fae9409b565
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2017-03-19 17:13:49 +0300

    Replace FirefoxAccountsCommands with WebChannelMessageToChrome

--- janus/static/common.js
+++ janus/static/common.js
@@ -1,19 +1,39 @@
-function send(command, data) {
+function send(command, data, messageId) {
+    // https://github.com/mozilla/fxa-content-server/blob/master/docs/relier-communication-protocols/fx-webchannel.md
+    if (!messageId) {
+        messageId = new Date().getTime().toString(36) + "-" + Math.random().toString(36).substr(2);
+    }
     var detail = {
-        command: command,
-        bubbles: true
+        id: "account_updates",
+        message: {
+            command: command,
+            messageId: messageId
+        }
     };
     if (typeof data !== "undefined") {
-        detail.data = data;
+        detail.message.data = data;
     }
-    // TODO: FirefoxAccountsCommands, now it's WebChannelMessageToChrome
-    // https://github.com/mozilla/fxa-content-server/blob/master/docs/relier-communication-protocols/fx-webchannel.md
-    var event = new window.CustomEvent("FirefoxAccountsCommand", {
-        detail: detail
+    var ua = navigator.userAgent,
+        uaFxIndex = ua.indexOf("Firefox/"),
+        uaFxVersion = uaFxIndex >= 0 ? ua.substr(uaFxIndex + 8) : null;
+    if (uaFxVersion !== null) {
+        var i = uaFxVersion.indexOf(" ");
+        if (i > 0) {
+            uaFxVersion = uaFxVersion.substr(0, i);
+        }
+        try {
+            uaFxVersion = parseFloat(uaFxVersion);
+        } catch (e) {
+            uaFxVersion = null;
+        }
+    }
+
+    var event = new window.CustomEvent("WebChannelMessageToChrome", {
+        detail: (uaFxVersion === null || uaFxVersion >= 50) ? JSON.stringify(detail) : detail
     });
     window.dispatchEvent(event);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-   send('loaded');
+   send('fxaccounts:loaded');
 });
